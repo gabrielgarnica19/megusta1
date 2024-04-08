e459ebbed9478082e58d5a66fe602f540e3f3a3c
Gabriel <gabriel.garnica.varas@institutocomercialosrno.cl>
2024-04-08 10:49:21 -0400

    primer commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function aumentarlikes1(){
+    var content1 = document.getElementById("like1")
+    var like1 =parseInt(content1.innerHTML)
+    content1.innerHTML = like1+ 1 + "like(s)"
+}
+function aumentarlikes2(){
+    var content2 = document.getElementById("like2")
+    var like2 =parseInt(content2.innerHTML)
+    content2.innerHTML = like2+ 1 + "like(s)"
+}
+function aumentarlikes3(){
+    var content3 = document.getElementById("like3")
+    var like3 =parseInt(content3.innerHTML)
+    content3.innerHTML = like3+ 1 + "like(s)"
+}
